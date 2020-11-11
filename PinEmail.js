6dffbd5d00a8645c407311bc8b5714916ffd3bba
sreeja99 <srijagodishala@gmail.com>
2020-11-11 14:28:44 +0530

    Added:Pincodes checked

---
+++ PinEmail.js
@@ -0,0 +1,11 @@
+let pincodePattern=RegExp('^[1-9]{1}[0-9]{5}|[1-9]{1}[0-9]{2}\\s[0-9]{3}');
+let emailPattern=RegExp("^[a-zA-Z0-9+_-]+([.][a-zA-Z0-9]+)*@([a-zA-Z0-9]+)([.][a-z]+)?[.][a-z]{2,}$");
+//uc1
+let pincode1=pincodePattern.test(400088);
+console.log(pincode1);
+//uc2
+let pincode2=pincodePattern.test(A400088);
+console.log(pincode2);
+//uc3
+let pincode3=pincodePattern.test(400088B);
+console.log(pincode3);
